b7d3bddbde3ad195762b03155324a4fede8d82a6
Sviatoslav Zalishchuk <szalishchuk@gmail.com>
2018-07-01 01:21:16 +0000

    Cleared up stuff that shouldn't be tracked and committed everything worth tracking at this point

---
+++ install_linux.sh
@@ -0,0 +1,65 @@
+
+
+# Move into the home directory first
+cd ~/
+
+# Install system dependencies
+pacman -S \
+  compton # standalone compositor to provide transitions when navigating between windows
+  ranger # file manager
+  rofi # a window switcher, application launcher and dmenu replacement
+  git # git client
+  gvim # On native vim in Arch there is no support for X so the +clipboard feature is missing, replace it with gvim.
+
+  pulseaudio # General-purpose sound server
+  pulseaudio-alsa # ALSA Configuration for PulseAudio
+  pulseaudio-bluetooth # Bluetooth support for PulseAudio
+  mpd # music player daemon
+  mpc # headless music player controlling playback with mpd
+  ncmpcpp # terminal GUI music player with playlist support, tag editor and other features, connecting to mpd for music playback
+  feh # image viewer
+  noto-fonts # collection of fonts that covers all the scripts encoded into Unicode
+  ttf-dejavu # fullfills some of the missing unicode symbols
+  chromium # web browser, open source version that Google Chrome derives from
+  libgnome-keyring # keychain service to store entered passwords
+  lastpass-cli # lastpass terminal client
+  cifs-utils # helps with mounting smb shares
+  vlc # primary GUI-based media player
+
+
+###---Install yaourt, AUR client---###
+# Make sure the standard repositories are up-to-date for anything that package-query or yaourt might need
+pacman -Sy
+# Pull the package-query package, a frontend to pacman's database and adds suport for AUR
+git clone https://aur.archlinux.org/package-query.git && cd package-query
+# Make the package. The -s flag syncs the plackage's standard repository dependencies (if it has any) with pacman before trying to build the package
+makepkg -si && cd ..
+# Pull the yaourt package
+git clone https://aur.archlinux.org/yaourt.git && cd yaourt
+# Make the package
+makepkg -si && cd ..
+
+###---Install necessary packages from AUR with yaourt ---###
+
+
+yaourt \
+  polybar-git # Status bar for the desktop environment
+  owncloud-client-git # ownCloud client application to sync all my working directory files
+  ttf-monaco # Preferrable font for the terminal
+  archey3 # Output Arch Linux logo with various system information
+  rofi-greenclip # Clipboard manager
+  buku # bookmarks manager
+  buku_run #rofi frontend for buku
+  ttf-ms-fonts # freely available MS fonts, including Times New Roman, Trebuchet MS, Webdings, Verdana, Andale Mono, Arial, Arial Black, Impact, Comic Sans MS, Courier New and Georgia
+  ephifonts 20180416-2 # A collection of free fonts including Helvetica, Futura and Gotham (ephifonts.com)
+
+
+###---Symbolic links----###
+
+ln -sf $HOME/.config/fonts/fonts.conf /etc/fonts/local.conf # Attach overrides for system-wide fonts configuration
+
+# Enable font presets 
+sudo ln -s /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+sudo ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+
---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# Launch bars for all available monitors
+for i in $(polybar -m | awk -F: '{print $1}');
+  do MONITOR=$i polybar --reload example -c $HOME/.config/polybar/config  & done
+
+# Set the wallpaper
+feh --bg-scale --no-xinerama ~/ownCloud/Wallpapers/agriculture-beautiful-view-city-park-1080722.jpg
+
+echo "Polybars have been launched"
