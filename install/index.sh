298912221181e3cd40152f5ad9f2655b2169ba04
sviat <szalishchuk@gmail.com>
2018-08-13 12:38:15 +0300

    Added installation of yay - AUR helper to install script

--- install/index.sh
+++ install/index.sh
@@ -1,5 +1,10 @@
 #/bin/bash
 
+# Install AUR helper
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
 # Create .config folder in $HOME directory and populate it with sviatco/dotconfig settings
 cd $HOME;
 mkdir .config && $1;
@@ -7,5 +12,4 @@ git clone https://github.com/sviatco/dotconfig
 
 # Create a public rsa key
 ssh-keygen
-
 
