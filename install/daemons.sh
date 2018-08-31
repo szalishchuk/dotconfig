ad23e365bf6372459aebeda8f670af049cbeb82d
sviat <szalishchuk@gmail.com>
2018-08-31 14:50:06 +0300

    Added running emacs daemon as systemd service to daemons.sh

--- install/daemons.sh
+++ install/daemons.sh
@@ -5,3 +5,7 @@ systemctl --user enable mpd.service
 
 # Start the daemon to syncronize system time with remote NTP servers
 timedatectl set-ntp true
+
+# Start emacs server to run as a daemon
+systemctl enable --user emacs
+systemctl start --user emacs
