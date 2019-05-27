b193bb898836e0b9304e74f0e4c73fdf4431e3c8
Sviat <szalishchuk@gmail.com>
2019-05-27 11:58:59 +0300

    Menubar redesign

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bars for all available monitors
 for i in $(polybar -m | awk -F: '{print $1}');
-  do MONITOR=$i polybar --reload example -c $HOME/.config/polybar/config  & done
+  do MONITOR=$i polybar --reload -c $HOME/.config/polybar/config main & done
 
 # Set the wallpaper
 feh --bg-scale --no-xinerama ~/ownCloud/Wallpapers/agriculture-beautiful-view-city-park-1080722.jpg
